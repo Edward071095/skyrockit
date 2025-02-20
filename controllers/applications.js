cbb1fd01e1e5f9fa81fb54bd89cae03354387f10
EdwardW710 <EdwardW710@yahoo.com>
2025-02-20 12:16:59 -0500

    form

---
+++ controllers/applications.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+// Routes
+//GET /users/:userId/applications
+// Index route
+router.get('/', async (req, res) => {
+    try {
+        res.render('applications/index.ejs');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+    });
+
+// GET /users/:userId/applications/new
+router.get('/new', async (req, res) => {
+    res.render('applications/new.ejs');
+});
+
+    
+
+
+
+
+
+module.exports = router;
